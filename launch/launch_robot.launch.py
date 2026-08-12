d77d7cfd60182b2c8bb4721e79e1d990ced21169
Rob1 <robin.tchordjallian@gmail.com>
2026-08-12 18:24:05 +0200

    add camera

--- launch/launch_robot.launch.py
+++ launch/launch_robot.launch.py
@@ -26,6 +26,12 @@ def generate_launch_description():
                 )]), launch_arguments={'use_sim_time': 'false', 'use_ros2_control': 'true'}.items()
     )
 
+    camera = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory(package_name),'launch','camera.launch.py'
+                )]), launch_arguments={'use_sim_time': 'false', 'use_ros2_control': 'true'}.items()
+    )
+
     robot_description = Command(['ros2 param get --hide-type /robot_state_publisher robot_description'])
 
     controller_params_file = os.path.join(get_package_share_directory(package_name),'config','my_controllers.yaml')
@@ -118,4 +124,5 @@ def generate_launch_description():
         twist_stamper,
         foxglove_bridge,
         bag_record,
+        camera,
     ])
